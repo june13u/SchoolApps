5d12fe38927d050a8c27dd750f15d6b968876d5b
Otani Jun <ojune16@gmail.com>
2018-05-06 09:39:56 +0900

    Initializrで初期化

---
+++ src/main/java/com/june13u/SchoolApps/SchoolAppsApplication.java
@@ -0,0 +1,12 @@
+package com.june13u.SchoolApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAppsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/june13u/SchoolApps/SchoolAppsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.june13u.SchoolApps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchoolAppsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
